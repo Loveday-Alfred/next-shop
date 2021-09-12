84c9c66f1444c61e97f7d5b1b6116b628d6bda1a
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2021-09-11 21:12:04 -0700

    basics

--- firstPythonCode/main.py
+++ firstPythonCode/main.py
@@ -1,2 +1,31 @@
-print("Hello python")
-print("*" * 10)
+# print("Hello python")
+# print("+" * 10)
+#
+# # variables
+# price = 20
+# rating = 4.9
+# isPublished = False
+# name = 'Loveday'
+# print(price, rating, isPublished, name)
+#
+# # variable exercise
+# names = 'John Smith'
+# age = 20
+# isNewPatient = True
+# print(names, age, isNewPatient)
+#
+# # inputs
+# inp = input("What is ur name? ")
+# print("Hi " + inp)
+#
+# namess = input("What is ur name? ")
+# color = input("What is ur fav color? ")
+# print(namess + " likes " + color)
+#
+# # Type conversion
+# birthYear = input('BirthYear? ')
+# age = 2021 - int(birthYear)
+# print(age)
+
+# Conversion Exercise
+user = print('What is ur weight in pounds? ')
