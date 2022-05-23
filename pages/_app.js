8a54dba97514032f41b6e7fc50a2266fb9529d41
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2022-05-23 13:47:50 +0100

    deployment

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { Layout } from "../components";
+import StateContext from "../context/StateContext";
+import { Toaster } from "react-hot-toast";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
